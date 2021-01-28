1337ae425dc195b22824ff6b59cfd5db3ba4d7d7
ConnorDanky <connorschooley@gmail.com>
2021-01-28 10:14:25 -0800

    added intents, added missing line of code to on_message to process commands, added role changing support

--- ori.py
+++ ori.py
@@ -1,9 +1,13 @@
+import discord
 from discord.ext import commands
 
 import util.io_util as io_util
 
+#adding intents
+intents = discord.Intents().all()
+
 # Create Ori instance
-ori = commands.Bot(command_prefix='!')
+ori = commands.Bot(command_prefix='!', intents = intents)
 
 
 # on_ready event
@@ -18,6 +22,8 @@ async def on_message(message):
     # don't respond to ourselves
     if message.author == ori.user:
         return  # Do nothing
+    # Do this or else commands won't work!
+    await ori.process_commands(message)
 
 
 # 'spaghetti' command
@@ -37,6 +43,16 @@ async def ud(ctx, *args):
 @ori.command()
 async def define(ctx, *args):
     await ctx.send('https://www.merriam-webster.com/dictionary/', '+'.join(args), sep='%20')
+
+
+#Starting support for the ability to change roles
+@ori.command()
+async def role(ctx, member: discord.Member):
+    cool_red = ctx.guild.get_role(804232490158915624)
+
+    await member.add_roles(cool_red)
+
+    await ctx.send(member.mention + " is now cool red! ")
 
 
 # Load auth token from 'auth.json'
